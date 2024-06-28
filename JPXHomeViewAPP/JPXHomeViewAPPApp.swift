9bc611be43c7f09fc494b34085bdbd4201da9de7
Sourav Das <iamsouravdaswade420@gmail.com>
2024-06-28 21:17:23 +0530

    Initial Commit

---
+++ JPXHomeViewAPP/JPXHomeViewAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPXHomeViewAPPApp.swift
+//  JPXHomeViewAPP
+//
+//  Created by sourav das on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JPXHomeViewAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
